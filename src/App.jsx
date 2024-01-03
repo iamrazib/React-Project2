c2b02881a742906d984969656ba51efb389c7349
Sk. Razibul Islam <iamrazib@gmail.com>
2024-01-04 01:26:06 +0600

    card layout modified

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,55 @@ import { Timeline } from "./components/Timeline";
 
 function App() {
   
+  const cardData = [
+    {
+      name: "Nike",
+      price: 200,
+      desc: "Nike, Inc. is an American athletic footwear and apparel corporation headquartered near Beaverton, Oregon, United States. ",
+      imageUrl:
+        "https://pngimg.com/uploads/running_shoes/running_shoes_PNG5805.png",
+    },
+    {
+      name: "Bata",
+      price: 300,
+      desc: "The Bata Corporation is a multinational footwear, apparel and fashion accessories manufacturer and retailer of Moravian origin, headquartered in Lausanne, Switzerland. ",
+      imageUrl:
+        "https://freepngimg.com/thumb/men%20shoes/14-men-shoes-png-image.png",
+    },
+    {
+      name: "Apex",
+      price: 400,
+      desc: "Apex Retail Chain is a Bangladeshi retailer of shoes, clothing, and other accessories, founded in 1997, serving the Bangladeshi market for over 20 years.",
+      imageUrl:
+        "https://www.tbsnews.net/sites/default/files/styles/very_big_1/public/images/2022/09/25/das.png",
+    },
+    {
+      name: "Lotto",
+      price: 600,
+      desc: "Lotto Sport Italia is born in 1973 in the footwear district of Montebelluna, Italy.",
+      imageUrl:
+        "https://cdn.shopify.com/s/files/1/0591/4234/0785/products/210732_1EM-02.jpg?v=1677079540",
+    },
+  ];
+
+  //console.log(cardData);
+
   return (
     <>
-      <h1 className="text-center text-3xl bg-red-200">Project 2</h1>
+      <h1 className="text-center text-3xl bg-red-200">React-Tailwind-DaisyUI :: Project 2</h1>
+      <p className="bg-sky-300 h-2"></p>
       <div className="flex mt-3 space-x-4 justify-center">
-        <Card />
-        <Card />
-        <Card />
+        {
+          cardData.map((shoe, i) =>(
+            <Card cardInfo={shoe} key={i} />
+          ))
+        }
+
+        {/* <Card cardTitle="Nike" />
+        <Card cardTitle="Bata" />
+        <Card cardTitle="Apex" />
+        <Card cardTitle="Lotto" /> */}
+
       </div>
       <div className="flex mt-5 mb-5 space-x-4 justify-center">
         <Timeline />
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,27 @@
 
-export const Card = () => {
+export const Card = ({cardInfo}) => {
+
+  //console.log(props);
+  const { name, desc, imageUrl, price } = cardInfo;
+
   return (
     <div>
-      <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card bg-base-100 shadow-xl">
         <figure>
-          <img src="/src/assets/lighthouse-island.avif" alt="Shoes" />
+          <img src={imageUrl} alt="Shoes" width={150} />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">
-            Scenario!
-            <div className="badge badge-secondary">NEW</div>
+          <h2 className="card-title">{name}
+            {/* <div className="badge badge-secondary">NEW</div> */}
           </h2>
-          <p>
-            Orange sunset at lighthouse on the island of
-            tenerife, canary islands. spain
-          </p>
-          <div className="card-actions justify-end">
+          <p>Price: ${ price}</p>
+          <p>{desc}</p>
+          {/* <div className="card-actions justify-end">
             <div className="badge badge-outline">Fashion</div>
             <div className="badge badge-outline">Products</div>
+          </div> */}
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
           </div>
         </div>
       </div>
